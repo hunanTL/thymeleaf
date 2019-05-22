5abd46379c17eaa73a6b5a9c64e51496aa4cf09d
hunanTL <13647493750@163.com>
2019-05-22 11:06:32 +0800

    tthh

--- src/main/java/com/example/tl/Application.java
+++ src/main/java/com/example/tl/Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
+        System.out.println("th创造");
         SpringApplication.run(Application.class, args);
     }
 
